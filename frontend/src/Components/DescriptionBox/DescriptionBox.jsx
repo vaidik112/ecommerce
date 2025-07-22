b12d72318169aa19faef4d84c4c544c104004acb
Vaidik <vaidikkumar01@example.com>
2025-07-22 07:15:10 +0530

    Fixed: Added frontend as a normal folder

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">
+            Description
+        </div>
+        <div className="descriptionbox-nav-box fade">
+            Reviews (122)
+        </div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Upgrade your wardrobe with our Wooly, designed for ultimate comfort and timeless style. Made from 100% breathable cotton, this shirt is perfect for casual outings, work, or lounging at home.
+         </p>
+         <p>
+         At Shopper, we are passionate about bringing you the best products at unbeatable prices. With a wide range of categories, high-quality items, and a commitment to customer satisfaction, we strive to make your shopping experience seamless and enjoyable.
+
+We believe in:
+✔ Quality products
+✔ Affordable pricing
+✔ Exceptional customer service
+
+Join thousands of happy customers today!
+         </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
